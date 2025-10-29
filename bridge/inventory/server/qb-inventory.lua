027da16b743730ae0290785d214c0444f9a0114b
Butter Chilly <88847062+Butterchilly@users.noreply.github.com>
2025-10-29 15:59:48 +0530

    chore(bridge/inventory): updated inv bridge

--- bridge/inventory/server/qb-inventory.lua
+++ bridge/inventory/server/qb-inventory.lua
@@ -1,12 +1,15 @@
-if GetResourceState('qb-inventory') ~= 'started' then return end
-Debug('^1[Bridge] ^2qb-inventory detected')
+local inventoryName = nil
+if (GetResourceState('qb-inventory') == "started") then inventoryName = 'qb-inventory' end
+if (GetResourceState('ps-inventory') == "started") then inventoryName = 'ps-inventory' end
+if (GetResourceState('lj-inventory') == "started") then inventoryName = 'lj-inventory' end
+if not inventoryName then return end
 
 ---Add Inventory Item
 ---@param source integer | number
 ---@param item string
 ---@param amount number
 function AddItem(source, item, amount)
-    return exports['qb-inventory']:AddItem(source, item, amount)
+    return exports[inventoryName]:AddItem(source, item, amount)
 end
 
 ---Can Carry Inventory Item
@@ -14,5 +17,5 @@ end
 ---@param item string
 ---@param amount number
 function CanCarryItem(source, item, amount)
-    return exports['qb-inventory']:CanAddItem(source, item, amount)
+    return exports[inventoryName]:CanAddItem(source, item, amount)
 end
